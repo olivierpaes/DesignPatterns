1eb3bd8e4724bc9f91552be5ce93bfef89d95529
Olivier Paesbrugghe <olivier.paesbrugghe@gmail.com>
2017-08-24 23:33:03 +0200

    Added some comments to Frenchman class

--- Rtp.Factory/Frenchman.cs
+++ Rtp.Factory/Frenchman.cs
@@ -2,9 +2,16 @@
 {
     public  abstract partial class Person
     {
+        /// <summary>
+        /// This class is protected and an inner class so it is not possible to use the constructor from putside
+        /// </summary>
         protected class Frenchman: Person
         {
-            protected internal Frenchman() 
+            /// <summary>
+            /// Constructor is protected internal to make sure that it is useable by the Person class. 
+            /// Only nternal should also do but only protected won't do the trick.
+            /// </summary>
+            protected internal  Frenchman() 
                 : base(new SocialSecurityNumberValidationStrategyForFrance())
             {
                 Language = "French";
